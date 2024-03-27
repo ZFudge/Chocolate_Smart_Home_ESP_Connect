d29cdbb50cb3c07560740a3ce77dcd8a9b1dcd44
ZFudge <zacheryfudge+github@gmail.com>
2024-03-27 14:35:01 -0600

    implement base-level and controllertype-level messages

--- controller_data.h
+++ controller_data.h
@@ -2,29 +2,46 @@
 #define CONTROLLER_DATA_H_
 
 struct ControllerData {
-  String newDataReceivedTopic;
-  String stateRequestedTopic;
+  uint8_t mqttId;
+  String controllerType;
   String name;
 
-  String (*getStateMessage)();
+  String newDataReceivedTopic;
+  String stateRequestedTopic;
 
-  void init(const char* controllerType,
-            uint8_t mqttId,
+  String getStateMessageBase() {
+    return String(this->mqttId) + ',' +
+           String(this->controllerType) + ',' +
+           String(this->name);
+  };
+
+  String (*getStateMessageTypeImplementation)();
+
+  String getStateMessage() {
+    return getStateMessageBase() + ',' +
+     this->getStateMessageTypeImplementation();
+  };
+
+  void init(uint8_t mqttId,
+            const char* controllerType,
             String name,
-            String (*getStateMessage)()
+            String (*getStateMessageTypeImplementation)()
   ) {
-    randomSeed(ESP.getCycleCount());
+    this->mqttId = mqttId;
+    this->controllerType = controllerType;
 
+    randomSeed(ESP.getCycleCount());
     // Appending a randomly generated integer to the controller name
     // helps the server determine when a reboot has occurred.
     this->name = name + " - " + String(random(0, 999));
+
+    this->getStateMessageTypeImplementation = getStateMessageTypeImplementation;
 
     this->stateRequestedTopic  = "/request_device_state/" +
                                  String(mqttId) + "/";
     this->newDataReceivedTopic = "/" + String(controllerType) +
                                  "/" + String(mqttId) + "/";
 
-    this->getStateMessage = getStateMessage;
   }
 };
 
