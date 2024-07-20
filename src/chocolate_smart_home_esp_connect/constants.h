2659d4a3bcd1b36ba786213a099b14c8594497c3
zfudge <zacheryfudge+GitHub@gmail.com>
2024-07-20 13:19:42 -0600

    esp conditional definitions

--- src/chocolate_smart_home_esp_connect/constants.h
+++ src/chocolate_smart_home_esp_connect/constants.h
@@ -1,7 +1,12 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
+#if defined(ESP32)
+#define LED_BUILTIN       2
+#endif
+
 #define DEFAULT_MQTT_PORT 1883
+
 
 const char ALL_CONTROLLERS_STATES_REQUESTED_TOPIC[]  = "/request_devices_state/";
 const char SEND_THIS_CONTROLLERS_DATA_TOPIC[]        = "/receive_device_state/";
